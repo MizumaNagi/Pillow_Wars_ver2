ef99e429df08659b2cae7c02c70bd49bc83cb8e2
miku <mikudragonred@gmail.com>
2021-06-02 17:25:44 +0900

    新HPゲージに更新

--- PillowWar/Assets/WorkFolder/Sasaki/Script/UI.cs
+++ PillowWar/Assets/WorkFolder/Sasaki/Script/UI.cs
@@ -5,40 +5,56 @@ using UnityEngine.UI;
 
 public class UI : MonoBehaviour
 {
-    [SerializeField] private Slider[] hp;
-    [SerializeField] private Image[] image;
+    //[SerializeField] private Slider[] hp;
+    [SerializeField] private Image[] Futontimage;
     [SerializeField] private Text[] Futontext;
     [SerializeField] private Text Titlereturntext;
     [SerializeField] private Text Gamereturntext;
+    [SerializeField] private Transform[] hpIconParents;
 
     public UnityEngine.UI.Text Pausetext;
-    public List<float> playerhp = new List<float>();
+    //public List<float> playerhp = new List<float>();
+    private int iconChild = 0;
+    private List<List<Image>> hpIcons = new List<List<Image>>();
 
     // Start is called before the first frame update
     void Start()
     {
         Pausetext.enabled = false;
 
-        for (int i = 0; i < GameManager.Instance.joinPlayers; i++)
-        {
-            playerhp.Add(0);
-        }
+        iconChild = hpIconParents[0].childCount;
+        hpicon();
+
+        //for (int i = 0; i < GameManager.Instance.joinPlayers; i++)
+        //{
+        //    playerhp.Add(0);
+        //}
     }
 
     // Update is called once per frame
     void Update()
     {
-
         for (int i = 0; i < GameManager.Instance.joinPlayers; i++)
         {
-            playerhp[i] = 0;
-            playerhp[i] = (float)PlayerManager.Instance.charaDatas[i].HP / (float)GameManager.Instance.ruleData.maxHp;
+            float playerhp = (float)PlayerManager.Instance.charaDatas[i].HP / (float)GameManager.Instance.ruleData.maxHp;
 
-            hp[i].value = playerhp[i];
-            Pause();
-            Image();
+            for (int j = 0; j < iconChild; j++)
+            {
+                hpIcons[i][j].fillAmount = playerhp * iconChild - 1 * j;
+            }
         }
+        Pause();
+        Image();
+
+        //for (int i = 0; i < GameManager.Instance.joinPlayers; i++)
+        //{
+        //    playerhp[i] = 0;
+        //    playerhp[i] = (float)PlayerManager.Instance.charaDatas[i].HP / (float)GameManager.Instance.ruleData.maxHp;
+
+        //    hp[i].value = playerhp[i];
+        //}
     }
+
     public void Pause()
     {
         if (GameManager.Instance.isPause == true)
@@ -57,18 +73,33 @@ public class UI : MonoBehaviour
 
     public void Image()
     {
-        for (int i = 0; i < image.Length; i++)
+        for (int i = 0; i < Futontimage.Length; i++)
         {
             if (PlayerManager.Instance.charaDatas[i].isInBed == true)
             {
                 Futontext[i].enabled = true;
-                image[i].enabled = true;
+                Futontimage[i].enabled = true;
             }
             else
             {
                 Futontext[i].enabled = false;
-                image[i].enabled = false;
+                Futontimage[i].enabled = false;
             }
         }
     }
+
+    private void hpicon()
+    {
+
+        for (int i = 0; i < GameManager.Instance.joinPlayers; i++)
+        {
+            List<Image> images = new List<Image>();
+            for (int j = 0; j < iconChild; j++)
+            {
+                images.Add(hpIconParents[i].GetChild(j).GetComponent<Image>());
+            }
+            hpIcons.Add(images);
+        }
+    }
+
 }
