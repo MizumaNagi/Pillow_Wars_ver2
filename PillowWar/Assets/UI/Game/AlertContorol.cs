2c5adca2185b78c03e1d5a4990417d38d1ebdbf0
sakairock <jinsei-wa-rock0202@ezweb.ne.jp>
2021-07-16 13:22:42 +0900

    途中経過

---
+++ PillowWar/Assets/UI/Game/AlertContorol.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//DefaultExecutionOrder(-100)]
+public class AlertContorol : MonoBehaviour
+{
+    [SerializeField] GameObject AlertPanel;
+    [SerializeField] GameObject TimerPanel;
+
+    public Text timertext;
+    private int seconds;
+
+    private bool EventTrigger = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameEventScript.Instance.isEventStart == true)
+        {
+            GameEventScript.Instance.remainEventActiveTime -= Time.deltaTime;
+            seconds = (int)GameEventScript.Instance.remainEventActiveTime;
+            timertext.text = seconds.ToString();
+            if (EventTrigger == false)
+            {
+                EventTrigger = true;
+                AlertPanel.SetActive(true);
+                TimerPanel.SetActive(true);
+                StartCoroutine(Corutine()); 
+            }
+        }
+        if(GameEventScript.Instance.remainEventActiveTime <= 5)
+        {
+            TimerPanel.SetActive(false);
+        }
+        else if(GameEventScript.Instance.remainEventActiveTime <= 0)
+        {
+            EventTrigger = false;
+            Debug.Log("aaa");
+        }
+    }
+
+    private IEnumerator Corutine()
+    {
+        yield return new WaitForSeconds(3f);
+
+        AlertPanel.SetActive(false);
+    }
+}
