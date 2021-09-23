1bb6b1bbe12d7b4c9a6b370d40c6dc47d429fbe0
MizumaNagi <Hoge@mail.com>
2021-09-24 01:00:12 +0900

    バフアイテム情報UI追加

--- PillowWar/Assets/WorkFolder/Mizuma/Scripts/Game/TriggerGetItem.cs
+++ PillowWar/Assets/WorkFolder/Mizuma/Scripts/Game/TriggerGetItem.cs
@@ -1,12 +1,12 @@
-
-
-using System.Collections;
-using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TriggerGetItem : MonoBehaviour
 {
+    [SerializeField] private GameObject[] buffUIsP2;
+    [SerializeField] private GameObject[] buffUIsP4;
+
     private int itemIndex;
 
     private void Start()
@@ -18,18 +18,33 @@ public class TriggerGetItem : MonoBehaviour
     {
         if(other.CompareTag("Player"))
         {
+            // IDチェックS
             StringBuilder charaName = new StringBuilder(other.gameObject.name);
             charaName.Replace("Player", "");
             charaName.Replace("Npc", "");
 
             int charaIndex = int.Parse(charaName.ToString());
 
+            // 対象のData取得
             CharacterData data;
             if (charaIndex >= 100) data = PlayerManager.Instance.npcDatas[charaIndex - 100];
             else data = PlayerManager.Instance.playerDatas[charaIndex];
 
+            // バフ適用
             int rndIndex = Random.Range(0, itemIndex);
             data.buffInfo.GetItem(GameManager.Instance.itemData.itemInfos[rndIndex]);
+
+            // UI表示
+            if (charaIndex >= 100) return;
+
+            if(GameManager.Instance.joinPlayers == 2)
+            {
+                buffUIsP2[charaIndex].SetActive(true);
+            }
+            else
+            {
+                buffUIsP4[charaIndex].SetActive(true);
+            }
         }
     }
 }
---
+++ PillowWar/Assets/WorkFolder/Mizuma/Scripts/UI/BuffTextUpdate.cs
@@ -0,0 +1,90 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Text;
+using System.Collections;
+
+public class BuffTextUpdate : MonoBehaviour
+{
+    [SerializeField] private int characterNum;
+
+    private CharacterData characterData;
+    private Text buffText;
+    private RectTransform rect;
+
+    private Vector3 defaultPosP2 = new Vector3(-713, 160, 0);
+    private Vector3[] defaultPosP4 = {
+        new Vector3(266, -104, 0),
+        new Vector3(-313, -104, 0),
+        new Vector3(230, 436, 0),
+        new Vector3(-313, 436, 0),
+    };
+    private Vector3 laterTimePosP2 = new Vector3(-485, -220, 0);
+    private Vector3[] laterTimePosP4 ={
+        new Vector3(480, -485, 0),
+        new Vector3(-480, -485, 0),
+        new Vector3(480, 55, 0),
+        new Vector3(-480, 55, 0),
+    };
+
+    private void Start()
+    {
+        characterData = PlayerManager.Instance.playerDatas[characterNum];
+        buffText = GetComponentInChildren<Text>();
+        rect = GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        if (characterData.buffInfo.remainBigPillowTime > 0)
+        {
+            buffText.text = $"枕サイズ上昇 {characterData.buffInfo.remainBigPillowTime.ToString("0")}秒";
+        }
+        else if (characterData.buffInfo.remainDoubleDmgCount > 0)
+        {
+            buffText.text = $"ダメージ上昇 {characterData.buffInfo.remainDoubleDmgCount.ToString("0")}回";
+        }
+        else if (characterData.buffInfo.remainFastSpdTime > 0)
+        {
+            buffText.text = $"移動速度上昇 {characterData.buffInfo.remainFastSpdTime.ToString("0")}秒";
+        }
+        else if (characterData.buffInfo.remainFastThrowTime > 0)
+        {
+            buffText.text = $"投擲速度上昇 {characterData.buffInfo.remainFastThrowTime.ToString("0")}秒";
+        }
+        else
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    private IEnumerator DelayMoveUI()
+    {
+        yield return new WaitForSeconds(2f);
+        if (GameManager.Instance.joinPlayers == 2)
+        {
+            if (characterNum == 0) rect.localPosition = defaultPosP2 + new Vector3(0, -267f, 0);
+            else rect.localPosition = defaultPosP2;
+        }
+        else
+        {
+            rect.localPosition = defaultPosP4[characterNum];
+        }
+    }
+
+    private void OnEnable()
+    {
+        if (rect == null) return;
+
+        if (GameManager.Instance.joinPlayers == 2)
+        {
+            if (characterNum == 0) rect.localPosition = laterTimePosP2 + new Vector3(0, -267f, 0);
+            else rect.localPosition = laterTimePosP2;
+        }
+        else
+        {
+            rect.localPosition = laterTimePosP4[characterNum];
+        }
+
+        StartCoroutine(DelayMoveUI());
+    }
+}
